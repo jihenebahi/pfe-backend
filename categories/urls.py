2e06286aebfc7a313eda8173dabc64c5ade5f53b
jihenebahi <jihenebahi@gmail.com>
2026-03-02 11:14:09 +0100

    message clair

--- categories/urls.py
+++ categories/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.liste_categories, name='liste_categories'),
+    path('ajouter/', views.ajouter_categorie, name='ajouter_categorie'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Categorie
+from .serializers import CategorieSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def liste_categories(request):
+    categories = Categorie.objects.all().order_by('date_creation')
+    serializer = CategorieSerializer(categories, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def ajouter_categorie(request):
+    serializer = CategorieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
